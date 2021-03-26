aaf99945e302d8a275bbe45f091ccd6c2f49aab2
Jared Andersen <j-mac@Jareds-MacBook-Pro.local>
2021-03-26 15:29:50 -0700

    Assignment 100% Complete

--- solutions.sql
+++ solutions.sql
@@ -146,3 +146,25 @@ where reports_to = 2;
 SELECT COUNT(city) FROM employee
 where city = 'Lethbridge';
 
+
+<!--Table - invoice Instructions 
+-- Count how many orders were made from the USA.
+SELECT COUNT(billing_country) FROM invoice
+WHERE billing_country ='USA';
+-- Find the largest order total amount.
+SELECT MAX(total) FROM invoice;
+-- Find the smallest order total amount.
+SELECT MAX(total) FROM invoice;
+-- Find all orders bigger than $5.
+SELECT * FROM invoice
+WHERE total > 5;
+-- Count how many orders were smaller than $5.
+SELECT COUNT(*) FROM invoice
+WHERE total < 5;
+-- Count how many orders were in CA, TX, or AZ (use IN).
+SELECT COUNT(*) FROM invoice
+WHERE billing_state IN('CA','TX','AZ');
+-- Get the average total of the orders.
+SELECT AVG(total) from invoice;
+-- Get the total sum of the orders.
+SELECT SUM(total) from invoice;
